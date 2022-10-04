3a9748d9402b1fc1e92a53ab2002f055a6abe4a3
Kanaiya <katarmal.kanaiya@gmail.com>
2022-10-05 01:34:53 +0530

    wip

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public void fakePage()
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb;User ID=sa;Password=demol23";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            cnn.Close();
+
+        }
+    }
+}
